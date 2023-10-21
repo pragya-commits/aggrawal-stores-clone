61bc46a0fbf31a49e13848becda39371639c32c7
pragya-commits <pendharkarp23@gmail.com>
2023-10-21 23:06:48 +0530

    completes nav bar

--- src/components/Header1.js
+++ src/components/Header1.js
@@ -1,11 +1,26 @@
 import { Box, Button, Typography } from "@mui/material";
 import React from "react";
+import Logo1 from "./aggrawal stores logo.png";
 
 const Header1 = () => {
   const styles = {
     mainheaddiv: {
       display: "flex",
       justifyContent: "space-between",
+      alignItems: "center",
+      margin: "50px",
+    },
+    navbar: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    navitem: {
+      marginRight: "20px",
+    },
+    btn: {
+      paddingRight: "25px",
+      paddingLeft: "25px",
     },
   };
 
@@ -14,15 +29,19 @@ const Header1 = () => {
       {/* div---> Box */}
       {/* className---> sx */}
       <Box sx={styles.mainheaddiv}>
-        <Box>LOGO</Box>
-
         <Box>
+          <img src={Logo1} width={90} height={50} />
+        </Box>
+
+        <Box sx={styles.navbar}>
           {/* (h1-h6,p,span)----> Typography */}
-          <Typography> CONTACT US</Typography>
-          <Typography> FIND NEAREST STORE</Typography>
-          <Typography> ORDER ONLINE</Typography>
+          <Typography sx={styles.navitem}> CONTACT US</Typography>
+          <Typography sx={styles.navitem}> FIND NEAREST STORE</Typography>
+          <Typography sx={styles.navitem}> ORDER ONLINE</Typography>
           {/* button---> Button */}
-          <Button variant="outlined">Sign In</Button>
+          <Button sx={styles.btn} variant="outlined">
+            Sign In
+          </Button>
         </Box>
       </Box>
     </div>
